56af7f7e4a173a3079cd3de06146b00da4712329
JimmyWu <gmwu185@gmail.com>
2022-05-06 18:35:59 +0800

    使用 mongoose 連線，區分本地與遠端依指令決定環境

---
+++ connections/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+require('dotenv').config();
+
+console.log('NODE_ENV', process.env.NODE_ENV);
+let DBPath = '';
+if (process.env.NODE_ENV === 'production') {
+  DBPath = process.env.DB_URL;
+  DBPath = DBPath.replace('<password>', process.env.DB_PASSWORD);
+  DBPath = DBPath.replace('myFirstDatabase', process.env.DB_NAME);
+} else if (process.env.NODE_ENV === 'development') {
+  DBPath = 'mongodb://localhost:27017/' + process.env.DB_NAME;
+}
+
+mongoose
+  .connect(DBPath)
+  .then((response) => {
+    console.log('mongoose link ok !!');
+  })
+  .catch((error) => {
+    console.log('mongoose link error', error);
+  });
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/posts', function (req, res, next) {
+  console.log('/posts req.url', req.url);
+  res.json({ title: 'json' });
+  res.end();
 });
 
 module.exports = router;
