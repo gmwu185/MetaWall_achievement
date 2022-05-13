469ff03d5a9e256b05ec082274e2a27a5d385ee7
JimmyWu <gmwu185@gmail.com>
2022-05-14 00:18:43 +0800

    增加 /posts API 使用文件與分類

--- swagger.js
+++ swagger.js
@@ -7,21 +7,21 @@ const doc = { // 生成資料、格式、設定
   },
   host: 'localhost:3000',
   schemes: ['http', 'https'],
-  securityDefinitions: {
-    apiKeyAuth: {
-      type: 'apiKey',
-      in: 'headers',
-      name: 'authorization',
-      description: '請加上 API Token'
-    }
-  },
+  // securityDefinitions: {
+  //   apiKeyAuth: {
+  //     type: 'apiKey',
+  //     in: 'headers',
+  //     name: 'authorization',
+  //     description: '請加上 API Token'
+  //   }
+  // },
   definitions: {
-    createdPosts: {
-      "$name": "createdPost--test 加入 swagger.js 中",
-      "content": "createdPost-test",
-      "tags": ["string-1", "string-2"],
-      "type": "string"
-    }
+    // createdPosts: {
+    //   // "$name": "createdPost--test 加入 swagger.js 中",
+    //   // "content": "createdPost-test",
+    //   // "tags": ["string-1", "string-2"],
+    //   // "type": "string"
+    // }
   }
 };
 const outputFile = './swagger-output.json'; // 使用套件生成文件讀取用的 JSON 資料檔
