4b9b030261587e8fc95c8af0c60e71920cdb295c
JimmyWu <gmwu185@gmail.com>
2022-05-13 23:27:22 +0800

    加入 swagger 套件與基本引用 express 設定

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
 
+/* router ------------------------------------------------------------------- */
 var indexRouter = require('./routes/index');
 var postsRouter = require('./routes/posts');
 var usersRouter = require('./routes/users');
+/* /router ------------------------------------------------------------------- */
 
+/* express 設定 --------------------------------------------------------------- */
 var app = express();
-
 require('./connections');
 
 app.use(cors());
@@ -22,5 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/users', usersRouter);
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+/* /express 設定 --------------------------------------------------------------- */
 
 module.exports = app;
