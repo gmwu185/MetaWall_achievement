7b8d77da08683c43159480f75fa8d5ad7cae5e6b
JimmyWu <gmwu185@gmail.com>
2022-06-08 22:08:01 +0800

    新增 FB newUser 查看進入 API 四個參數 console.log - 5

--- routes/tpAuth.js
+++ routes/tpAuth.js
@@ -24,7 +24,7 @@ passport.use(
       clientID: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
       callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-      profileFields: ['email', 'displayName'],
+      profileFields: ['emails', 'displayName'],
     },
     function (accessToken, refreshToken, profile, cb) {
       return cb(null, profile);
