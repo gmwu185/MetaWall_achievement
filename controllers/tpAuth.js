3b6356d83364497824d4f3149ac2519528a4762b
JimmyWu <gmwu185@gmail.com>
2022-06-09 00:08:23 +0800

    移除部份註解與 console

--- controllers/tpAuth.js
+++ controllers/tpAuth.js
@@ -1,5 +1,3 @@
-// const User = require("../models/usersModel");
-
 const { generateRedirectJWT } = require('../handStates/auth');
 const findOrCreateUser = require('../helps/findOrCreateUser');
 const handleError = require('../handStates/handleError');
@@ -7,13 +5,10 @@ const handleError = require('../handStates/handleError');
 const auth = {
   googleCallback: handleError(async (req, res) => {
     const user = await findOrCreateUser('google', req.user._json);
-    // res.status(200).json(user);
     generateRedirectJWT(user, res);
   }),
   facebookCallback: handleError(async (req, res) => {
-    console.log('req.user._json', req.user._json);
     const user = await findOrCreateUser('facebook', req.user._json);
-    // res.status(200).json(user);
     generateRedirectJWT(user, res);
   }),
 };
